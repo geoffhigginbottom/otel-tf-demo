cec26e35b4eb403e06cf6870aeb74f11f4efeac2
Geoff Higginbottom <ghigginbottom@splunk.com>
2024-12-20 15:35:21 +0000

    Create s3_iam_role.tf

---
+++ modules/instances/s3_iam_role.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "ec2_role" {
+#   name = "ec2_s3_access_role"
+  name = join("-", [var.environment, "ec2_s3_access_role"])
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
