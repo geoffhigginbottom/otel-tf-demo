c9d69251d3ec4a780cee92539f369813ca171086
Geoff Higginbottom <ghigginbottom@splunk.com>
2024-12-20 15:36:37 +0000

    Update vm_mysql_servers.tf

--- modules/instances/vm_mysql_servers.tf
+++ modules/instances/vm_mysql_servers.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "mysql" {
   subnet_id                 = "${var.public_subnet_ids[ count.index % length(var.public_subnet_ids) ]}"
   key_name                  = var.key_name
   vpc_security_group_ids    = [aws_security_group.instances_sg.id]
+  iam_instance_profile      = aws_iam_instance_profile.ec2_instance_profile.name
 
   tags = {
     Name = lower(join("-",[var.environment, "mysql", count.index + 1]))
@@ -20,17 +21,27 @@ resource "aws_instance" "mysql" {
 
   provisioner "file" {
     source      = "${path.module}/scripts/update_splunk_otel_collector_conf_mysql.sh"
-    destination = "/tmp/update_splunk_otel_collector_conf_mysql.sh"
+    destination = "/tmp/update_splunk_otel_collector_conf.sh"
   }
 
   provisioner "file" {
     source      = "${path.module}/config_files/mysql_agent_config.yaml"
-    destination = "/tmp/mysql_agent_config.yaml"
+    destination = "/tmp/agent_config.yaml"
   }
 
   provisioner "file" {
     source      = "${path.module}/scripts/install_splunk_universal_forwarder_mysql.sh"
-    destination = "/tmp/install_splunk_universal_forwarder_mysql.sh"
+    destination = "/tmp/install_splunk_universal_forwarder.sh"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/scripts/install_splunk_universal_forwarder_mysql_splunk_cloud.sh"
+    destination = "/tmp/install_splunk_universal_forwarder_splunk_cloud.sh"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/config_files/splunkclouduf.spl"
+    destination = "/tmp/splunkclouduf.spl"
   }
 
   provisioner "remote-exec" {
@@ -57,28 +68,57 @@ resource "aws_instance" "mysql" {
     
     # Install Otel Agent
       "sudo curl -sSL https://dl.signalfx.com/splunk-otel-collector.sh > /tmp/splunk-otel-collector.sh",
-      # "sudo sh /tmp/splunk-otel-collector.sh --realm ${var.realm}  -- ${var.access_token} --mode agent --without-fluentd --collector-version ${var.collector_version}",
       "sudo sh /tmp/splunk-otel-collector.sh --realm ${var.realm}  -- ${var.access_token} --mode agent --collector-version ${var.collector_version} --discovery",
+
       "sudo mv /etc/otel/collector/agent_config.yaml /etc/otel/collector/agent_config.bak",
-      "sudo mv /tmp/mysql_agent_config.yaml /etc/otel/collector/agent_config.yaml",
+      "sudo mv /tmp/agent_config.yaml /etc/otel/collector/agent_config.yaml",
       "sudo chmod +x /tmp/update_splunk_otel_collector_conf_mysql.sh",
       "sudo /tmp/update_splunk_otel_collector_conf_mysql.sh $LBURL $MYSQLUSER $MYSQLPWD",
 
-    ## Generate Vars
-      var.splunk_ent_count == "1" ? "UNIVERSAL_FORWARDER_FILENAME=${var.universalforwarder_filename}" : "echo skipping because Splunk Ent is not deployed",
-      var.splunk_ent_count == "1" ? "UNIVERSAL_FORWARDER_URL=${var.universalforwarder_url}" : "echo skipping because Splunk Ent is not deployed",
-      var.splunk_ent_count == "1" ? "PASSWORD=${random_string.splunk_password.result}" : "echo skipping because Splunk Ent is not deployed",
-      var.splunk_ent_count == "1" ? "SPLUNK_IP=${aws_instance.splunk_ent.0.private_ip}" : "echo skipping because Splunk Ent is not deployed",
+    # ## If splunk_ent_count = 0 then set a default value to prevent terraform errors
+    #   "SPLUNK_IP=${length(aws_instance.splunk_ent) > 0 ? aws_instance.splunk_ent[0].private_ip : "127.0.0.1"}",
 
-    ## Write env vars to file (used for debugging)
-      var.splunk_ent_count == "1" ? "echo $UNIVERSAL_FORWARDER_FILENAME > /tmp/UNIVERSAL_FORWARDER_FILENAME" : "echo skipping because Splunk Ent is not deployed",
-      var.splunk_ent_count == "1" ? "echo $UNIVERSAL_FORWARDER_URL > /tmp/UNIVERSAL_FORWARDER_URL" : "echo skipping because Splunk Ent is not deployed",
-      var.splunk_ent_count == "1" ? "echo $PASSWORD > /tmp/UNIVERSAL_FORWARDER_PASSWORD" : "echo skipping because Splunk Ent is not deployed",
-      var.splunk_ent_count == "1" ? "echo $SPLUNK_IP > /tmp/SPLUNK_IP" : "echo skipping because Splunk Ent is not deployed",
+    ## Splunk Ent Private IP now set as a var, sp above lines no longer needed
+      "SPLUNK_IP=${var.splunk_private_ip}",
 
-    ## Install Splunk Universal Forwarder
-      "sudo chmod +x /tmp/install_splunk_universal_forwarder_mysql.sh",
-      var.splunk_ent_count == "1" ? "/tmp/install_splunk_universal_forwarder_mysql.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD $SPLUNK_IP" : "echo skipping because Splunk Ent is not deployed",
+    ## Deloy UF for Splunk Ent, but only if splunk_ent_count = 1
+      <<EOT
+      if [ ${var.splunk_ent_count} -eq 1 ]; then
+        sudo chmod +x /tmp/install_splunk_universal_forwarder.sh
+        UNIVERSAL_FORWARDER_FILENAME=${var.universalforwarder_filename}
+        UNIVERSAL_FORWARDER_URL=${var.universalforwarder_url}
+        PASSWORD=${random_string.splunk_password.result}
+        /tmp/install_splunk_universal_forwarder.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD $SPLUNK_IP
+
+        ## Write env vars to file (used for debugging)
+        echo $UNIVERSAL_FORWARDER_FILENAME > /tmp/UNIVERSAL_FORWARDER_FILENAME
+        echo $UNIVERSAL_FORWARDER_URL > /tmp/UNIVERSAL_FORWARDER_URL
+        echo $PASSWORD > /tmp/UNIVERSAL_FORWARDER_PASSWORD
+        echo $SPLUNK_IP > /tmp/SPLUNK_IP
+      else
+        echo "Skipping as splunk_ent_count is 0"
+      fi
+      EOT
+      ,
+
+    ## Deloy UF for Splunk Cloud, but only if splunk_cloud_enabled = true
+      <<EOT
+      if [ "${var.splunk_cloud_enabled}" = "true" ]; then
+        sudo chmod +x /tmp/install_splunk_universal_forwarder_splunk_cloud.sh
+        UNIVERSAL_FORWARDER_FILENAME=${var.universalforwarder_filename}
+        UNIVERSAL_FORWARDER_URL=${var.universalforwarder_url}
+        PASSWORD=${random_string.splunk_password.result}
+        /tmp/install_splunk_universal_forwarder_splunk_cloud.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD
+
+        ## Write env vars to file (used for debugging)
+        echo $UNIVERSAL_FORWARDER_FILENAME > /tmp/UNIVERSAL_FORWARDER_FILENAME
+        echo $UNIVERSAL_FORWARDER_URL > /tmp/UNIVERSAL_FORWARDER_URL
+        echo $PASSWORD > /tmp/UNIVERSAL_FORWARDER_PASSWORD
+      else
+        echo "Skipping as splunk_cloud_enabled is false"
+      fi
+      EOT
+      ,
     ]
   }
 
