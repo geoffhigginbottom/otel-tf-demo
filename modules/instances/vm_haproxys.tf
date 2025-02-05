ae0a71c3dc833d199e7134d53bfbcc769f736090
Geoff Higginbottom <ghigginbottom@splunk.com>
2025-02-05 13:27:37 +0000

    Update vm_haproxys.tf

--- modules/instances/vm_haproxys.tf
+++ modules/instances/vm_haproxys.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "haproxy" {
   }
   ebs_block_device {
     device_name = "/dev/xvdg"
-    volume_size = 10
+    volume_size = 8
     volume_type = "gp2"
   }
   key_name                  = var.key_name
@@ -21,7 +21,7 @@ resource "aws_instance" "haproxy" {
     splunkit_environment_type = "non-prd"
     splunkit_data_classification = "public"
   }
- 
+
   provisioner "file" {
     source      = "${path.module}/scripts/install_haproxy.sh"
     destination = "/tmp/install_haproxy.sh"
@@ -63,27 +63,31 @@ resource "aws_instance" "haproxy" {
       "sudo echo 'type=83' | sudo sfdisk /dev/xvdg",
       "sudo mkfs.ext4 /dev/xvdg1",
       "sudo mount /dev/xvdg1 /media/data",
+      "sudo mkdir /media/data/logs",
+      "sudo mkdir /media/data/logs/otel",
 
       "TOKEN=${var.access_token}",
       "REALM=${var.realm}",
       "HOSTNAME=${self.tags.Name}",
       "LBURL=${aws_lb.gateway-lb.dns_name}",
-      
+
     ## Install HA Proxy
       "sudo chmod +x /tmp/install_haproxy.sh",
       "sudo /tmp/install_haproxy.sh",
-    
+
     ## Install Otel Agent
       "sudo curl -sSL https://dl.signalfx.com/splunk-otel-collector.sh > /tmp/splunk-otel-collector.sh",
       "sudo sh /tmp/splunk-otel-collector.sh --realm ${var.realm}  -- ${var.access_token} --mode agent --collector-version ${var.collector_version} --discovery",
-      "sudo chmod +x /tmp/update_splunk_otel_collector.sh",
-      "sudo /tmp/update_splunk_otel_collector.sh $LBURL",
+
       "sudo mv /etc/otel/collector/agent_config.yaml /etc/otel/collector/agent_config.bak",
       "sudo mv /tmp/agent_config.yaml /etc/otel/collector/agent_config.yaml",
+      "sudo chmod +x /tmp/update_splunk_otel_collector.sh",
+      "sudo /tmp/update_splunk_otel_collector.sh $LBURL",
       "sudo chown splunk-otel-collector:splunk-otel-collector -R /media/data/logs",
       "sudo systemctl restart splunk-otel-collector",
+
     ## If splunk_ent_count = 0 then set a default value to prevent terraform errors
-      "SPLUNK_IP=${length(aws_instance.splunk_ent) > 0 ? aws_instance.splunk_ent[0].private_ip : "127.0.0.1"}",
+      # "SPLUNK_IP=${length(aws_instance.splunk_ent) > 0 ? aws_instance.splunk_ent[0].private_ip : "127.0.0.1"}",
 
     ## Deloy UF for Splunk Ent, but only if splunk_ent_count = 1
       <<EOT
@@ -92,13 +96,16 @@ resource "aws_instance" "haproxy" {
         UNIVERSAL_FORWARDER_FILENAME=${var.universalforwarder_filename}
         UNIVERSAL_FORWARDER_URL=${var.universalforwarder_url}
         PASSWORD=${random_string.splunk_password.result}
-        /tmp/install_splunk_universal_forwarder.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD $SPLUNK_IP
+        SPLUNK_IP=${var.splunk_private_ip}
+        PRIVATE_DNS=${self.private_dns}
+        /tmp/install_splunk_universal_forwarder.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD $SPLUNK_IP $PRIVATE_DNS
 
         ## Write env vars to file (used for debugging)
         echo $UNIVERSAL_FORWARDER_FILENAME > /tmp/UNIVERSAL_FORWARDER_FILENAME
         echo $UNIVERSAL_FORWARDER_URL > /tmp/UNIVERSAL_FORWARDER_URL
         echo $PASSWORD > /tmp/UNIVERSAL_FORWARDER_PASSWORD
         echo $SPLUNK_IP > /tmp/SPLUNK_IP
+        echo $PRIVATE_DNS > /tmp/PRIVATE_DNS
       else
         echo "Skipping as splunk_ent_count is 0"
       fi
@@ -112,12 +119,14 @@ resource "aws_instance" "haproxy" {
         UNIVERSAL_FORWARDER_FILENAME=${var.universalforwarder_filename}
         UNIVERSAL_FORWARDER_URL=${var.universalforwarder_url}
         PASSWORD=${random_string.splunk_password.result}
-        /tmp/install_splunk_universal_forwarder_splunk_cloud.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD
+        PRIVATE_DNS=${self.private_dns}
+        /tmp/install_splunk_universal_forwarder_splunk_cloud.sh $UNIVERSAL_FORWARDER_FILENAME $UNIVERSAL_FORWARDER_URL $PASSWORD $PRIVATE_DNS
 
         ## Write env vars to file (used for debugging)
         echo $UNIVERSAL_FORWARDER_FILENAME > /tmp/UNIVERSAL_FORWARDER_FILENAME
         echo $UNIVERSAL_FORWARDER_URL > /tmp/UNIVERSAL_FORWARDER_URL
         echo $PASSWORD > /tmp/UNIVERSAL_FORWARDER_PASSWORD
+        echo $PRIVATE_DNS > /tmp/PRIVATE_DNS
       else
         echo "Skipping as splunk_cloud_enabled is false"
       fi
