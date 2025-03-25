96f934c09438dd59810ae327e896d5201bfde089
Geoff Higginbottom <ghigginbottom@splunk.com>
2025-03-25 16:40:01 +0000

    removed delete option

--- modules/s3/s3_sync.tf
+++ modules/s3/s3_sync.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "sync_config_files" {
   provisioner "local-exec" {
-    command = "aws s3 sync ./config_files/ s3://${var.s3_bucket_name}/config_files/ --delete"
+    command = "aws s3 sync ./config_files/ s3://${var.s3_bucket_name}/config_files/"
   }
 
   triggers = {
@@ -10,7 +10,7 @@ resource "null_resource" "sync_config_files" {
 
 resource "null_resource" "sync_scripts" {
   provisioner "local-exec" {
-    command = "aws s3 sync ./scripts/ s3://${var.s3_bucket_name}/scripts/ --delete"
+    command = "aws s3 sync ./scripts/ s3://${var.s3_bucket_name}/scripts/"
   }
 
   triggers = {
@@ -20,7 +20,7 @@ resource "null_resource" "sync_scripts" {
 
 resource "null_resource" "sync_non_pub_files" {
   provisioner "local-exec" {
-    command = "aws s3 sync ./non_public_files/ s3://${var.s3_bucket_name}/non_public_files/ --delete"
+    command = "aws s3 sync ./non_public_files/ s3://${var.s3_bucket_name}/non_public_files/"
   }
 
   triggers = {
