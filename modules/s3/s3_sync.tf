a2b0c711a2c7f9622e9f7bd2f99c16ec874b6839
Geoff Higginbottom <ghigginbottom@splunk.com>
2025-03-25 18:10:16 +0000

    Update s3_sync.tf
    Enabled true sync with delete option, changed triggers to use a folder_hash

--- modules/s3/s3_sync.tf
+++ modules/s3/s3_sync.tf
@@ -1,29 +1,29 @@
 resource "null_resource" "sync_config_files" {
   provisioner "local-exec" {
-    command = "aws s3 sync ./config_files/ s3://${var.s3_bucket_name}/config_files/"
+    command = "aws s3 sync ./config_files/ s3://${var.s3_bucket_name}/config_files/ --delete"
   }
 
   triggers = {
-    always_run = timestamp()
+    folder_hash = md5(join("", fileset("./config_files/", "**")))
   }
 }
 
 resource "null_resource" "sync_scripts" {
   provisioner "local-exec" {
-    command = "aws s3 sync ./scripts/ s3://${var.s3_bucket_name}/scripts/"
+    command = "aws s3 sync ./scripts/ s3://${var.s3_bucket_name}/scripts/ --delete"
   }
 
   triggers = {
-    always_run = timestamp()
+    folder_hash = md5(join("", fileset("./scripts/", "**")))
   }
 }
 
 resource "null_resource" "sync_non_pub_files" {
   provisioner "local-exec" {
-    command = "aws s3 sync ./non_public_files/ s3://${var.s3_bucket_name}/non_public_files/"
+    command = "aws s3 sync ./non_public_files/ s3://${var.s3_bucket_name}/non_public_files/ --delete"
   }
 
   triggers = {
-    always_run = timestamp()
+    folder_hash = md5(join("", fileset("./non_public_files/", "**")))
   }
 }
