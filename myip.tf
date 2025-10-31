96da2664db12aa3522c0bc115fb97a9d9636af3f
Geoff Higginbottom <geoffhigginbottom@users.noreply.github.com>
2025-10-31 17:10:43 +0000

    Removed Output

--- myip.tf
+++ myip.tf
@@ -2,6 +2,6 @@ data "http" "my_public_ip" {
   url = "https://ipv4.icanhazip.com/"
 }
 
-output "my_ip_addr" {
-  value = "${chomp(data.http.my_public_ip.response_body)}"
-}
+# output "my_ip_addr" {
+#   value = "${chomp(data.http.my_public_ip.response_body)}"
+# }
