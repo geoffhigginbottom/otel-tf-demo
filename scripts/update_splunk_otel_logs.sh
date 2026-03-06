85809db366bb9f733a67677e0e0e12195925c8dd
Geoff Higginbottom <geoffhigginbottom@users.noreply.github.com>
2026-03-06 16:20:00 +0000

    Replaced OTLP Endpoint with Variable

--- scripts/update_splunk_otel_logs.sh
+++ scripts/update_splunk_otel_logs.sh
@@ -1,34 +1,33 @@
 #! /bin/bash
-# Version 1.0
-## Configure the otel agent to send Logs to Splunk via HEC
+# Version 1.1
+## Configure the otel agent to send Logs to Splunk via HEC and OTLP
 
 SPLUNK_FQDN=$1
 HEC_TOKEN=$2
 
-# Check if the SPLUNK_FQDN is provided
-if [ -z "$SPLUNK_FQDN" ]; then
-  echo "Usage: $0 <SPLUNK_FQDN>"
+# Check if both SPLUNK_FQDN and HEC_TOKEN are provided
+if [ -z "$SPLUNK_FQDN" ] || [ -z "$HEC_TOKEN" ]; then
+  echo "Usage: $0 <SPLUNK_FQDN> <HEC_TOKEN>"
   exit 1
 fi
+
+# Path to the config file
+CONFIG_FILE="/etc/otel/collector/splunk-otel-collector.conf"
 
 # Create backup of the original configuration file
 cp /etc/otel/collector/splunk-otel-collector.conf /etc/otel/collector/splunk-otel-collector.otel_logs_bak
 
 
-# Path to the config file
-CONFIG_FILE="/etc/otel/collector/splunk-otel-collector.conf"
-
-# New values
-NEW_URL="https://${SPLUNK_FQDN}:8088/services/collector"
-NEW_TOKEN="${HEC_TOKEN}"
-
 # Replace SPLUNK_HEC_URL
-sed -i "s|^SPLUNK_HEC_URL=.*|SPLUNK_HEC_URL=${NEW_URL}|" "$CONFIG_FILE"
+sed -i "s|^SPLUNK_HEC_URL=.*|SPLUNK_HEC_URL=https://${SPLUNK_FQDN}:8088/services/collector|" "$CONFIG_FILE"
 
 # Replace SPLUNK_HEC_TOKEN
-sed -i "s|^SPLUNK_HEC_TOKEN=.*|SPLUNK_HEC_TOKEN=${NEW_TOKEN}|" "$CONFIG_FILE"
+sed -i "s|^SPLUNK_HEC_TOKEN=.*|SPLUNK_HEC_TOKEN=${HEC_TOKEN}|" "$CONFIG_FILE"
 
-echo SPLUNK_FILE_STORAGE_EXTENSION_PATH=/tmp >> "$CONFIG_FILE"
+
+# Append additional configuration lines
+echo "SPLUNK_FILE_STORAGE_EXTENSION_PATH=/tmp" >> "$CONFIG_FILE"
+echo "SPLUNK_OTLP_URL=${SPLUNK_FQDN}" >> "$CONFIG_FILE"
 
 # Restart the Splunk OpenTelemetry Collector service to apply changes
 systemctl restart splunk-otel-collector
