89904022bc30e94ee9419cd27a9582da19267674
Geoff Higginbottom <geoffhigginbottom@users.noreply.github.com>
2026-07-23 16:02:08 +0100

    added rules for opamp

--- modules/instances/loadbalancer-listeners.tf
+++ modules/instances/loadbalancer-listeners.tf
@@ -24,6 +24,31 @@ resource "aws_lb_listener_rule" "rule-4318" {
   }
 }
 
+resource "aws_lb_listener" "gateway-lb-listener-4320" {
+  default_action {
+    target_group_arn = aws_lb_target_group.gateway-lb-tg-4320.arn
+    type = "forward"
+  }
+  load_balancer_arn = aws_lb.gateway-lb.arn
+  port = 4320
+  protocol = "HTTP"
+}
+
+resource "aws_lb_listener_rule" "rule-4320" {
+  listener_arn = aws_lb_listener.gateway-lb-listener-4320.id
+  priority = 100
+
+  action {
+    target_group_arn = aws_lb_target_group.gateway-lb-tg-4320.arn
+    type = "forward"
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static/*"]
+    }
+  }
+}
 
 resource "aws_lb_listener" "gateway-lb-listener-9943" {
   default_action {
--- modules/instances/loadbalancer-target-groups.tf
+++ modules/instances/loadbalancer-target-groups.tf
@@ -22,6 +22,31 @@ resource "aws_lb_target_group_attachment" "gateway-lb-tg-4318-attachment" {
   port             = 4318
 }
 
+
+resource "aws_lb_target_group" "gateway-lb-tg-4320" {
+  name     = "gateway-lb-tg-4320"
+  port     = 4320
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  
+  health_check {
+    path = "/"
+    port = 13133
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
+}
+
+resource "aws_lb_target_group_attachment" "gateway-lb-tg-4320-attachment" {
+  count            = var.gateway_count
+  target_group_arn = aws_lb_target_group.gateway-lb-tg-4320.arn
+  target_id        = aws_instance.gateway[count.index].id
+  port             = 4320
+}
+
 resource "aws_lb_target_group" "gateway-lb-tg-9943" {
   name     = "gateway-lb-tg-9943"
   port     = 9943
